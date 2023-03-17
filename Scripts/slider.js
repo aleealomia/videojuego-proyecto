0114a59e84d9d515015147ace1fb275d0734b1f2
aleealomia <alejandra.arjona.0905@gmail.com>
2023-03-17 01:17:21 -0600

    slider

---
+++ Scripts/slider.js
@@ -0,0 +1,37 @@
+var i = 0;
+var arr = ["url('./imagenes/slide1.jfif')","url('./imagenes/slide2.jfif')","url('./imagenes/slide3.jfif')","url('./imagenes/slide4.jfif')","url('./imagenes/slide5.jfif')"];
+var arrtexto = ["imagen1","Imagen2","Imagen3","Imagen4","Imagen5"]
+function prevf(){
+    
+    if (i > 0) {
+        i--;
+    }
+    else {
+        i = arr.length-1;
+    }
+    slider.style.backgroundImage = arr[i] ;
+    textoAbajo.innerText = arrtexto[i];
+
+    console.log(arr[i])
+}
+function nextf(){
+    if (i < arr.length-1) {
+        i++;
+    }
+    else {
+        i = 0;
+    }
+    slider.style.backgroundImage = arr[i];
+    textoAbajo.innerText = arrtexto[i];
+    console.log(arr[i])
+}
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var slider = document.querySelector(".slider-img");
+var textoAbajo = document.getElementById("textoabajo")
+prev.addEventListener("click",prevf);
+next.addEventListener("click",nextf);
+
+prevf();
+nextf();
+
